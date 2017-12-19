15fa7b1840a34a6d34c182c35b7e93c4ae2a36f6
Jordan Johnson <jordanleo7@hotmail.com>
2017-12-19 11:20:07 -0600

    Added axios, concurrently dependencies, and updated CORS in server.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './Navbar.css';
 
 class Navbar extends Component {
   render() {
 
-    function handleClick(e) {
-      e.preventDefault();
-      console.log('+ was clicked.');
-      fetch('http://localhost:3001/boards', {method:"POST"});
+    function handleClick() {
+      axios.post('http://localhost:3000/api/boards', {
+        name: 'kanban'
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+         if (error.response) {
+           // The request was made and the server responded with a status code
+           // that falls out of the range of 2xx
+           console.log(error.response.data);
+           console.log(error.response.status);
+           console.log(error.response.headers);
+         } else if (error.request) {
+           // The request was made but no response was received
+           // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+           // http.ClientRequest in node.js
+           console.log(error.request);
+         } else {
+           // Something happened in setting up the request that triggered an Error
+           console.log('Error', error.message);
+         }
+         console.log(error.config);
+       });
     }
-
 
     return (
       <nav className="Navbar">
@@ -18,9 +39,9 @@ class Navbar extends Component {
         </a>
         <ul>
           <li>
-            <a href="#" onClick={handleClick}>
+            <button onClick={handleClick}>
               +
-            </a>
+            </button>
           </li>
         </ul>
       </nav>
--- src/server.js
+++ src/server.js
@@ -3,8 +3,10 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const urlEncodedParser = bodyParser.urlencoded({ extended: false });
 const router = express.Router();
-
+var Board = require('./data/models/Board');
 const app = express();
+
+app.set("port", process.env.PORT || 3001);
 
 // Import Mongoose
 const mongoose = require('mongoose');
@@ -26,26 +28,26 @@ db.once('open', function() {
 
 
 
-
-if (!process.env.DISABLE_XORIGIN) {
-  app.use(function(req, res, next) {
-    var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
-    var origin = req.headers.origin || '*';
-    if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
-         console.log(origin);
-         res.setHeader('Access-Control-Allow-Origin', origin);
-         res.header("Access-Control-Allow-Headers", "GET, PUT, POST, DELETE", "Origin, X-Requested-With, Content-Type, Accept");
-    }
-    next();
-  });
-}
-
-
-
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
+
+
+
+
+
+//To prevent errors from Cross Origin Resource Sharing, we will set our headers to allow CORS with middleware like so:
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+
+  next();
+});
+
+
 
 
 // test http://localhost:3001/ping
@@ -53,11 +55,13 @@ app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 
-var Board = require('./data/models/Board');
+app.get('/', function(req, res) {
+  res.json('you did it');
+});
 
-app.post('/boards', function(req, res, next) {
+app.post('/api/boards', function(req, res, next) {
 
-  var boardName = 'test';
+  var boardName = 'test2';
 
   var createBoard = new Board({
     name: boardName
@@ -66,11 +70,11 @@ app.post('/boards', function(req, res, next) {
   Board.create(createBoard).then(function(){
     console.log('board created');
   })
-  res.send({type: 'POST'})
+  return res.send('board created')
 })
 
 
 
-var listener = app.listen(process.env.PORT || 3001, function () {
-  console.log('Server is listening on port ' + listener.address().port || 3001);
+app.listen(app.get("port"), () => {
+  console.log('Server is listening on port ' + app.get("port"));
 });
